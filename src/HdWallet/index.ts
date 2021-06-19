12b2624cb0de4fd8282504e9613d7ef5a4333a68
tomijaga <jagatomi12@gmail.com>
2021-06-19 03:36:41 +0000

    HdWallet Collection

--- src/HdWallet/index.ts
+++ src/HdWallet/index.ts
@@ -1,1 +1,28 @@
-export {default as HdWallet} from "./hdWalletCollection"
+import { Address, Account, CoinDetails, Curve, Path, Hex, MAX_ADDRESS_GAP } from "../models";
+
+import { BaseHdWallet } from "./baseHd";
+import { WebHdWallet } from "./webHd";
+
+import { BtcHdWallet } from "./bitcoin";
+import { EthHdWallet } from "./ethereum";
+import { TnbHdWallet } from "./thenewboston";
+
+interface HdWalletCollection{
+  [crypto: string]: (mnemonicOrSeed: string, ...otherOptions: any[])=> BaseHdWallet| WebHdWallet
+}
+
+export const HdWallet: HdWalletCollection =  {
+  /**
+   * Default HdWallet for Custom Coin derivations
+   */
+  default: (mnemonicOrSeed: string, coinDetails: CoinDetails) => new BaseHdWallet(mnemonicOrSeed, coinDetails),
+
+  bitcoin: (mnemonicOrSeed: string) => new BtcHdWallet(mnemonicOrSeed) as WebHdWallet,
+
+  ethereum: (mnemonicOrSeed: string) => new EthHdWallet(mnemonicOrSeed) as WebHdWallet,
+
+  thenewboston: (mnemonicOrSeed: string, nodeUrl?: string) => {
+    if (!nodeUrl) return new TnbHdWallet(mnemonicOrSeed, nodeUrl) as BaseHdWallet
+    return new TnbHdWallet(mnemonicOrSeed, nodeUrl) as WebHdWallet
+  }
+}
